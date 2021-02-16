3dfa7b4d50e96976fe356b5f54cda15d7ad6c8d2
AgostinoMatt <AgostinoMatt@users.noreply.github.com>
2021-02-16 15:15:08 -0500

    initial

---
+++ infinite/Assets/Scripts/levelgenerater.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class levelgenerater : MonoBehaviour
+{
+    
+    [SerializeField] private Transform level_Start;
+    [SerializeField] private Transform levelPart_1;
+
+    private Vector3 lastEndPosition;
+
+    private void Awake()
+    {
+        lastEndPosition = level_Start.Find("EndPosition").position;
+        SpawnLevelPart();
+    }
+
+    private void SpawnLevelPart()
+    {
+        Transform lastLevelPartTransform = SpawnLevelPart(lastEndPosition);
+        lastEndPosition = lastLevelPartTransform.Find("EndPosition").position;
+    }
+
+    private Transform SpawnLevelPart(Vector3 spawnPosition)
+    {
+        Transform levelPartTransform = Instantiate(levelPart_1, spawnPosition, Quaternion.identity);
+        return levelPartTransform;
+    }
+}
