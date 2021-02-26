c58894ee15c4a0d44849447187a4d9e307a26324
AgostinoMatt <AgostinoMatt@users.noreply.github.com>
2021-02-26 18:46:56 -0500

    fire works

---
+++ infinite/Assets/Scripts/FireTimer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireTimer : MonoBehaviour
+{
+    public float timer;
+
+    
+    void Update()
+    {
+        timer += Time.deltaTime;
+        Debug.Log(timer);
+        if (timer >= 2)
+        {
+            this.gameObject.SetActive(false);
+            timer = 0 + Time.deltaTime;
+            Invoke("FireOn", 2);
+        }
+            
+    }
+
+    void FireOn()
+    {
+        this.gameObject.SetActive(true);
+    }
+}
