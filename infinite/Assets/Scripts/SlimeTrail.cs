4de9cf9a5d28994e14ea52bf4875515b29c41544
AgostinoMatt <AgostinoMatt@users.noreply.github.com>
2021-02-27 09:57:40 -0500

    slime trail
    slime trail works follows speed increase

--- infinite/Assets/Scripts/SlimeTrail.cs
+++ infinite/Assets/Scripts/SlimeTrail.cs
@@ -4,15 +4,14 @@ using UnityEngine;
 
 public class SlimeTrail : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float speed = 1.0f;
+    public float speedIncrease = 0.1f;
 
     // Update is called once per frame
     void Update()
     {
-        
+        ParticleSystem ps = GetComponent<ParticleSystem>();
+        var main = ps.main;
+        main.simulationSpeed = speed += speedIncrease * Time.deltaTime;
     }
 }
