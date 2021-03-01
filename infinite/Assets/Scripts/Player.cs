22d7fdebf3a87089c46117dd4b26411e4f38319c
AgostinoMatt <AgostinoMatt@users.noreply.github.com>
2021-03-01 16:32:24 -0500

    anims
    left and right

--- infinite/Assets/Scripts/Player.cs
+++ infinite/Assets/Scripts/Player.cs
@@ -6,11 +6,33 @@ public class Player : MonoBehaviour
 {
     public float moveSpeed = 3f;
     GameObject player;
-    
+
+    private Animator anim;
+
+    private void Start()
+    {
+        anim = this.GetComponentInChildren<Animator>();
+    }
 
     private void FixedUpdate()
     {
         float axis = Input.GetAxis("Horizontal");
+
+
+
+        if(axis >= 0.1)
+        {
+            anim.SetBool("right", true);
+        }
+        else if (axis <= -0.1)
+        {
+            anim.SetBool("left", true);
+        }
+        else
+        {
+            anim.SetBool("left", false);
+            anim.SetBool("right", false);
+        }
 
         if (axis != 0)
         {
@@ -24,9 +46,5 @@ public class Player : MonoBehaviour
     {
         return transform.position;
     }
-
-    private void OnTriggerEnter(Collider other)
-    {
-        
-    }
+    
 }
