d6b1f193f27ca39f91cc7e84a912c936651d8a96
alexxquan <41336089+alexxquan@users.noreply.github.com>
2021-03-11 16:13:26 -0500

    Settings Menu
    settings menu fixed and cleaned up

---
+++ infinite/Assets/Scripts/Menu/SettingsMenu.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class SettingsMenu : MonoBehaviour
+{
+    public AudioMixer audioMixer;
+
+    public void SetVolume (float volume)
+    {
+        audioMixer.SetFloat("volume", volume);
+        Debug.Log(volume);
+    }
+
+    public void SetSFX (bool sfxToggle)
+    {
+        audioMixer.FindMatchingGroups("SFX");
+        audioMixer.SetFloat("sfxVolume", 0);
+        Debug.Log(sfxToggle);
+    }
+}
